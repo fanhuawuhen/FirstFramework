77e04d8951bd25cec7e4a6150d6652c1b1caefbc
张旭 <572786979@qq.com>
2019-02-20 21:46:29 +0800

    firstFramework

---
+++ FirstFramework/FirstClass.swift
@@ -0,0 +1,18 @@
+//
+//  FirstClass.swift
+//  FirstFramework
+//
+//  Created by 张旭 on 2019/2/20.
+//  Copyright © 2019 张旭. All rights reserved.
+//
+
+import Foundation
+
+public class FirstClass {
+    public static func printHelloWorld() {
+        print("Hello World")
+    }
+    static func notReveaFunc() {
+        print("Don't revea")
+    }
+}
